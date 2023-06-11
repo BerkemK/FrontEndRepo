afbbcbe217999064d1e403a9fe94d5437a44c3aa
JWLaessig <s0573711@htw-berlin.de>
2023-06-11 13:08:53 +0200

    commit 1. erster schritt zum meilenstein 4 - vorlage vom prof übernommen

--- HelloWorld/src/App.vue
+++ HelloWorld/src/App.vue
@@ -1,85 +1,64 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/login" v-if="!authenticated">
+      Login
+    </router-link> |
+    <router-link to="/profile" v-if="authenticated" >
+      Profile
+    </router-link> |
+    <a v-if="authenticated" v-on:click="logout()">
+      Logout
+    </a>
+  </div>
+  <router-view/>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script>
+export default {
+  name: 'app',
+  data: function () {
+    return { authenticated: false }
+  },
+  async created () {
+    await this.isAuthenticated()
+    this.$auth.authStateManager.subscribe(this.isAuthenticated)
+  },
+  watch: {
+    // Everytime the route changes, check for auth status
+    $route: 'isAuthenticated'
+  },
+  methods: {
+    async isAuthenticated () {
+      this.authenticated = await this.$auth.isAuthenticated()
+    },
+    async logout () {
+      await this.$auth.signOut()
+    }
+  }
 }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  margin-top: 2rem;
+  color: #2c3e50;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+#nav {
+  padding: 30px;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- HelloWorld/src/views/HomeView.vue
+++ HelloWorld/src/views/HomeView.vue
@@ -1,9 +1,42 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="home" class="home">
+    <div v-if="!this.$root.authenticated">
+      <p>Hello, Vue.</p>
+      <router-link role="button" to="/login">
+        Login
+      </router-link>
+    </div>
+
+    <div v-if="this.$root.authenticated">
+      <p>Welcome back, {{claims.name}}!</p>
+      <p>
+        You have successfully authenticated with Okta!
+        Visit the <a href="/profile">My Profile</a> page to take a look inside the ID token.
+      </p>
+    </div>
+    <DynamicForm title="Let's talk to the backend!"/>
+  </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import DynamicForm from '@/components/DynamicForm.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    DynamicForm
+  },
+  data: function () {
+    return {
+      claims: ''
+    }
+  },
+  created () { this.setup() },
+  methods: {
+    async setup () {
+      if (this.$root.authenticated) { this.claims = await this.$auth.getUser() }
+    }
+  }
+}
+</script>
