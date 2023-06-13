6e9e804322465abe6303acfdf0256106396443a9
JWLaessig <s0573711@htw-berlin.de>
2023-06-13 09:59:03 +0200

    commit 4. Meilenstein 4 fertig

--- HelloWorld/src/views/Home.vue
+++ HelloWorld/src/views/Home.vue
@@ -1,19 +1,5 @@
 <template>
   <div id="home" class="home">
-    <div v-if="!this.$root.authenticated">
-      <p>Hello, Vue.</p>
-      <router-link role="button" to="/login">
-        Login
-      </router-link>
-    </div>
-
-    <div v-if="this.$root.authenticated">
-      <p>Welcome back, {{claims.name}}!</p>
-      <p>
-        You have successfully authenticated with Okta!
-        Visit the <a href="/profile">My Profile</a> page to take a look inside the ID token.
-      </p>
-    </div>
     <DynamicForm title="Let's talk to the backend!"/>
   </div>
 </template>
