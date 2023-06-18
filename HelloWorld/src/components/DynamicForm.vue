ec1ea91e60d69597023507788eed2b8c07421069
JWLaessig <s0573711@htw-berlin.de>
2023-06-18 16:18:21 +0200

    commit 10. delete eingefügt

--- HelloWorld/src/components/DynamicForm.vue
+++ HelloWorld/src/components/DynamicForm.vue
@@ -1,6 +1,7 @@
 <template>
   <h3>{{ title }}</h3>
   <div>
+    <button type="button" @click="deleteDatabase()">Delete Database</button>
     <input v-model="nameField" placeholder="Name" type="text" ref="nameInput">
     <input v-model="alcGehaltField" placeholder="Alkoholgehalt" type="text" ref="alcGehaltInput">
     <input v-model="mlField" placeholder="Milliliter" type="text" ref="mlInput" @keyup.enter="save()">
@@ -16,18 +17,22 @@
         <th>Alkoholgehalt</th>
         <th>Milliliter</th>
         <th>nüchtern</th>
+        <th></th>
       </tr>
       </thead>
       <tbody>
       <tr v-if="items.length === 0">
-        <td colspan="2">No products yet</td>
+        <td colspan="5">No products yet</td>
       </tr>
       <tr v-for="item in myFilterFunc(filterCrit)" :key="item.id">
         <td>{{ formatDateForDisplay(item.getrunken) }}</td>
         <td>{{ item.name }}</td>
         <td>{{ item.alcGehalt }}</td>
         <td>{{ item.ml }}</td>
-        <td> {{ formatDateForDisplay(item.nuechtern) }}</td>
+        <td>{{ formatDateForDisplay(item.nuechtern) }}</td>
+        <td>
+          <button type="button" @click="deleteItem(item.id)">Delete</button>
+        </td>
       </tr>
       <tr>
         <td>{{ nameField }}</td>
@@ -41,6 +46,7 @@
 
 <script>
 import moment from 'moment';
+
 export default {
   name: 'DynamicForm',
   props: ['title'],
@@ -53,80 +59,101 @@ export default {
       claims: '',
       accessToken: '',
       filterCrit: ''
-    }
+    };
   },
   methods: {
     myFilterFunc(crit) {
       return this.items.filter(
           it => crit.length < 1 ||
               it.name.toLowerCase().includes(crit.toLowerCase())
-      )
+      );
     },
-    loadThings() {
-      //const baseUrl = process.env.VUE_APP_BACKEND_BASE_URL
-      //const email = this.claims.email
-      //const endpoint = baseUrl + '/drinks' + '?owner=' + email
-      const endpoint = 'http://localhost:8080/drinks'
+    async loadDrinks() {
+      const endpoint = 'http://localhost:8080/drinks';
       const requestOptions = {
         method: 'GET',
         redirect: 'follow'
-        // headers: {
-        //   Authorization: 'Bearer ' + this.accessToken
-        // }
+      };
+      try {
+        const response = await fetch(endpoint, requestOptions);
+        const result = await response.json();
+        this.items = result.map(drink => ({
+          ...drink,
+          getrunken: moment(drink.getrunken, 'YYYY-MM-DDTHH:mm:ss').format('YYYY-MM-DD HH:mm:ss'),
+          nuechtern: moment(drink.nuechtern, 'YYYY-MM-DDTHH:mm:ss').format('YYYY-MM-DD HH:mm:ss')
+        }));
+      } catch (error) {
+        console.log('error', error);
       }
-      fetch(endpoint, requestOptions)
-          .then(response => response.json())
-          .then(result => {
-            this.items = result.map(drink => ({
-              ...drink,
-              getrunken: moment(drink.getrunken, 'YYYY-MM-DDTHH:mm:ss').format('YYYY-MM-DD HH:mm:ss'),
-              nuechtern: moment(drink.nuechtern, 'YYYY-MM-DDTHH:mm:ss').format('YYYY-MM-DD HH:mm:ss')
-            }))
-          })
-          .catch(error => console.log('error', error))
     },
     formatDateForDisplay(dateString) {
       const formattedDate = moment(dateString).format('DD.MM.YYYY HH:mm:ss');
       return formattedDate;
     },
-    save() {
-      //const baseUrl = process.env.VUE_APP_BACKEND_BASE_URL
-      //const endpoint = baseUrl + '/drinks'
-      const endpoint = 'http://localhost:8080/drinks'
+    async save() {
+      const endpoint = 'http://localhost:8080/drinks';
       const data = {
         name: this.nameField,
         alcGehalt: this.alcGehaltField,
-        ml: this.mlField,
-        //owner: this.claims.email
-      }
+        ml: this.mlField
+      };
       const requestOptions = {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
-          // Authorization: 'Bearer ' + this.accessToken
         },
         body: JSON.stringify(data)
+      };
+      try {
+        const response = await fetch(endpoint, requestOptions);
+        const responseData = await response.json();
+        console.log('Success:', responseData);
+        await this.loadDrinks();
+      } catch (error) {
+        console.log('error', error);
       }
-      fetch(endpoint, requestOptions)
-          .then(response => response.json())
-          .then(data => {
-            console.log('Success:', data)
-          })
-          .catch(error => console.log('error', error))
+    },
+    async deleteItem(itemId) {
+      const endpoint = `http://localhost:8080/drinks/${itemId}`;
+      const requestOptions = {
+        method: 'DELETE'
+      };
+      try {
+        const response = await fetch(endpoint, requestOptions);
+        const responseData = await response.json();
+        console.log('Success:', responseData);
+        await this.loadDrinks();
+      } catch (error) {
+        console.log('error', error);
+      }
+    },
+    async deleteDatabase() {
+      const endpoint = 'http://localhost:8080/drinks';
+      const requestOptions = {
+        method: 'DELETE'
+      };
+      try {
+        const response = await fetch(endpoint, requestOptions);
+        const responseData = await response.json();
+        console.log('Success:', responseData);
+        this.items = [];
+        await this.loadDrinks();
+      } catch (error) {
+        console.log('error', error);
+      }
     },
     async setup() {
       if (this.$root.authenticated) {
-        this.claims = await this.$auth.getUser()
-        // this.accessToken = await this.$auth.getAccessToken()
+        this.claims = await this.$auth.getUser();
       }
     }
   },
   async created() {
-    await this.setup()
-    this.loadThings()
+    await this.setup();
+    await this.loadDrinks();
   },
   mounted() {}
-}
+};
 </script>
 
 <style scoped>
