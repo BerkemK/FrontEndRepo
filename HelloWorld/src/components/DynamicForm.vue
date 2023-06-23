1b12549a7609ac4b8507ab7d689e12483a527789
BerkemK <Berkem.Kalkan@Student.HTW-Berlin.de>
2023-06-23 17:16:27 +0200

    Delete Button und style.css hinzugefügt

--- HelloWorld/src/components/DynamicForm.vue
+++ HelloWorld/src/components/DynamicForm.vue
@@ -1,10 +1,11 @@
 <template>
   <h3>{{ title }}</h3>
   <div>
+    <button type="button" @click="deleteAll()">Delete All</button><br>
     <input v-model="nameField" placeholder="Name" type="text" ref="nameInput">
     <input v-model="alcGehaltField" placeholder="Alkoholgehalt" type="text" ref="alcGehaltInput">
     <input v-model="mlField" placeholder="Milliliter" type="text" ref="mlInput" @keyup.enter="save()">
-    <button type="button" @click="save()">Save</button>
+    <button type="button" @click="save()">Save</button><br>
     <input v-model="filterCrit" placeholder="Filterkriterium">
   </div>
   <div>
@@ -16,11 +17,12 @@
         <th>Alkoholgehalt</th>
         <th>Milliliter</th>
         <th>nüchtern</th>
+        <th></th> <!-- Added column for delete buttons -->
       </tr>
       </thead>
       <tbody>
       <tr v-if="items.length === 0">
-        <td colspan="2">No products yet</td>
+        <td colspan="6">No products yet</td>
       </tr>
       <tr v-for="item in myFilterFunc(filterCrit)" :key="item.id">
         <td>{{ formatDateForDisplay(item.getrunken) }}</td>
@@ -28,6 +30,9 @@
         <td>{{ item.alcGehalt }}</td>
         <td>{{ item.ml }}</td>
         <td>{{ formatDateForDisplay(item.nuechtern) }}</td>
+        <td>
+          <button type="button" @click="deleteItem(item.id)">Delete</button>
+        </td>
       </tr>
       <tr>
         <td>{{ nameField }}</td>
@@ -41,6 +46,7 @@
 
 <script>
 import moment from 'moment';
+
 export default {
   name: 'DynamicForm',
   props: ['title'],
@@ -53,89 +59,106 @@ export default {
       claims: '',
       accessToken: '',
       filterCrit: ''
-    }
+    };
   },
   methods: {
     myFilterFunc(crit) {
       return this.items.filter(
-          it => crit.length < 1 ||
+          it =>
+              crit.length < 1 ||
               it.name.toLowerCase().includes(crit.toLowerCase())
-      )
+      );
     },
     loadThings() {
-      //const baseUrl = process.env.VUE_APP_BACKEND_BASE_URL
-      //const email = this.claims.email
-      //const endpoint = baseUrl + '/drinks' + '?owner=' + email
-      const endpoint = 'http://localhost:8080/drinks'
+      const endpoint = 'http://localhost:8080/drinks';
       const requestOptions = {
         method: 'GET',
         redirect: 'follow'
-        // headers: {
-        //   Authorization: 'Bearer ' + this.accessToken
-        // }
-      }
+      };
       fetch(endpoint, requestOptions)
           .then(response => response.json())
           .then(result => {
             this.items = result.map(drink => ({
               ...drink,
-              getrunken: moment(drink.getrunken, 'YYYY-MM-DDTHH:mm:ss').format('YYYY-MM-DD HH:mm:ss'),
-              nuechtern: moment(drink.nuechtern, 'YYYY-MM-DDTHH:mm:ss').format('YYYY-MM-DD HH:mm:ss')
-            }))
+              getrunken: moment(drink.getrunken, 'YYYY-MM-DDTHH:mm:ss').format(
+                  'YYYY-MM-DD HH:mm:ss'
+              ),
+              nuechtern: moment(drink.nuechtern, 'YYYY-MM-DDTHH:mm:ss').format(
+                  'YYYY-MM-DD HH:mm:ss'
+              )
+            }));
           })
-          .catch(error => console.log('error', error))
+          .catch(error => console.log('error', error));
     },
     formatDateForDisplay(dateString) {
       const formattedDate = moment(dateString).format('DD.MM.YYYY HH:mm:ss');
       return formattedDate;
     },
     save() {
-      //const baseUrl = process.env.VUE_APP_BACKEND_BASE_URL
-      //const endpoint = baseUrl + '/drinks'
-      const endpoint = 'http://localhost:8080/drinks'
+      const endpoint = 'http://localhost:8080/drinks';
       const data = {
         name: this.nameField,
         alcGehalt: this.alcGehaltField,
-        ml: this.mlField,
-        //owner: this.claims.email
-      }
+        ml: this.mlField
+      };
       const requestOptions = {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
-          // Authorization: 'Bearer ' + this.accessToken
         },
         body: JSON.stringify(data)
-      }
+      };
       fetch(endpoint, requestOptions)
           .then(response => response.json())
           .then(data => {
-            console.log('Success:', data)
+            console.log('Success:', data);
           })
-          .catch(error => console.log('error', error))
+          .catch(error => console.log('error', error));
+    },
+    deleteItem(itemId) {
+      const endpoint = `http://localhost:8080/drinks/${itemId}`;
+      const requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow'
+      };
+      fetch(endpoint, requestOptions)
+          .then(response => response.json())
+          .then(data => {
+            console.log('Item deleted:', data);
+            this.loadThings(); // Refresh the list after deletion
+          })
+          .catch(error => console.log('error', error));
+    },
+    deleteAll() {
+      const endpoint = `http://localhost:8080/drinks`;
+      const requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow'
+      };
+      fetch(endpoint, requestOptions)
+          .then(response => response.json())
+          .then(data => {
+            console.log('Item deleted:', data);
+            this.loadThings(); // Refresh the list after deletion
+          })
+          .catch(error => console.log('error', error));
     },
     async setup() {
       if (this.$root.authenticated) {
-        this.claims = await this.$auth.getUser()
-        // this.accessToken = await this.$auth.getAccessToken()
+        this.claims = await this.$auth.getUser();
       }
     }
   },
   async created() {
-    await this.setup()
-    this.loadThings()
+    await this.setup();
+    this.loadThings();
   },
   mounted() {}
-}
+};
 </script>
 
 <style scoped>
-table {
-  margin-left: auto;
-  margin-right: auto;
-}
+@import "style.css";
 
-button {
-  color: blue;
-}
+
 </style>
